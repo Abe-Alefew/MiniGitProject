66829b22a8c2303b3bd3f3b2d52a4e7c664e877b
boanergaes <mebitzeamanuel@gmail.com>
2025-06-18 21:43:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,55 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include "../src/commands/add.hpp"
 
-using namespace std;
 
-void handleBranchCommand(const string& repoPath, const vector<string>& args);
-void handleCheckoutCommand(const string& repoPath, const vector<string>& args);
+int initCommand();
+int addCommand(const std::string& filename);
+int commitCommand(const std::string& message);
+int logCommits();
+void handleBranchCommand(const std::string& repoPath, const std::vector<std::string>& args);
+void handleCheckoutCommand(const std::string& repoPath, const std::vector<std::string>& args);
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        cerr << "Usage: minigit <command> [args]\n";
+    
+    if (argc < 2)
+    {
+        std::cout << "Usage: minigit <command>\n";
         return 1;
     }
-    
-    string command = argv[1];
 
-    string repoPath = ".minigit/";
-    vector<string> args(argv + 1, argv + argc);
-
-    if (command == "add"){
-        if (argc < 3){
-            cerr << "Usage: minigit add <filename>\n";
+    const std::vector<std::string> args(argv + 1, argv + argc);
+    const std::string repoPath = ".minigit/";
+    std::string command = argv[1];
+    if (command == "init")
+    {
+        return initCommand();
+    } else if (command == "add"){
+        if(argc < 3) {
+            std::cerr<< "Usage: ./minigit add <file>\n";
             return 1;
         }
-        string filename = argv[2];
-        addFile(filename);
-        
+
+        return addCommand(argv[2]);
+    } else if(command == "commit"){
+        std::string message;
+        if(argc >= 4 && std::string(argv[2]) == "-m"){
+            message = argv[3];
+        } else {
+            std:: cerr << "Usage: ./minigit commit -m \"message\"\n";
+            return 1;
+        }
+        return commitCommand(message);
+    } else if (command == "log") {
+        return logCommits();
     } else if (command == "branch") {
         handleBranchCommand(repoPath, args);
-        
     } else if (command == "checkout") {
         handleCheckoutCommand(repoPath, args);
-        
-    } else
-    {
-        cerr << "Unknown command.\n";
+    } else {
+        std::cout << "Unknown command: " << command << "\n";
+        return 1;
     }
 
     return 0;
