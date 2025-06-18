af51ad643d25eaa0a91eef2a001c9fca4549a8d0
saronKB <saronkiflu3@gmail.com>
2025-06-18 12:09:38 +0300

    added log functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
-#include "../src/commands/add.hpp"
+#include <string>
 
-using namespace std;
+
+int initCommand();
+int addCommand(const std::string& filename);
+int commitCommand(const std::string& message);
+int logCommits();
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    
+    if (argc < 2)
     {
-        cerr << "Usage: minigit add <filename>\n";
+        std::cout << "Usage: minigit <command>\n";
         return 1;
     }
-
-    string command = argv[1];
-    string filename = argv[2];
-
-    if (command == "add")
+    
+    std::string command = argv[1];
+    if (command == "init")
     {
-        addFile(filename);
-    }
-    else
-    {
-        cerr << "Unknown command.\n";
+        return initCommand();
+    } else if (command == "add"){
+        if(argc < 3) {
+            std::cerr<< "Usage: minigit add <file>\n";
+            return 1;
+        }
+
+        return addCommand(argv[2]);
+    } else if(command == "commit"){
+        std::string message;
+        if(argc >= 4 && std::string(argv[2]) == "-m"){
+            message = argv[3];
+        } else {
+            std:: cerr << "Usage: minigit commit -m \"message\"\n";
+            return 1;
+        }
+        return commitCommand(message);
+    } else if (command == "log") {
+        return logCommits();
     }
 
-    return 0;
+
+    std::cout << "Unknown command: " << command << "\n";
+    return 1;
 }
